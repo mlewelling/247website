63c5dbeb88986747deb3e18a02593ac3c2d4b34d
mlewelling <Michael@Michaels-MacBook-Pro.local>
2015-02-04 02:59:42 -0700

    added jsfiles

---
+++ js/headers.js
@@ -0,0 +1,41 @@
+	function openfirstHeader() {
+		var list = document.getElementById("firstSection");
+		
+		if (list.style.display == "none"){
+			list.style.display = "block";
+		}else{
+			list.style.display = "none";
+			}
+		}
+
+function opensecondHeader() {
+		var list = document.getElementById("secondSection");
+		
+		if (list.style.display == "none"){
+			list.style.display = "block";
+		}else{
+			list.style.display = "none";
+			}
+		}
+		
+function openthirdHeader() {
+		var list = document.getElementById("thirdSection");
+		
+		if (list.style.display == "none"){
+			list.style.display = "block";
+		}else{
+			list.style.display = "none";
+			}
+		}
+		
+function openfourthHeader() {
+		var list = document.getElementById("fourthSection");
+		
+		if (list.style.display == "none"){
+			list.style.display = "block";
+		}else{
+			list.style.display = "none";
+			}
+		}
+				
+		
